a2a9f05d876521f29ad4a304c89a8b54ca77187d
Eugene Millerman <e.v.millerman@gmail.com>
2014-03-22 18:41:36 -0400

    Add basic grid functionality and calculations

--- lib/heatgrid/grid.rb
+++ lib/heatgrid/grid.rb
@@ -1,18 +1,37 @@
+require 'pry'
 module Heatgrid
   class Grid
     attr_accessor :original_grid, :normalized_grid
+    @max
     
     def initialize(in_array)
       self.original_grid = in_array
-      normalized_grid = normalize(in_array)
+      self.normalized_grid = normalize(in_array)
     end
 
-    def to_rgb_grid
+    def rgb_grid
+      normalized_grid.map do |row|
+        row.map do |val|
+          normalized_to_rgb(val)
+        end
+      end
+    end
 
+    def normalized_to_rgb(num)
+      if (0..0.5).include?(num)
+        [0,(255 * (num*2)).to_i,(255*((0.5-num)*2)).to_i]
+      else
+        [(255 * (num-0.5)*2).to_i,(255*(1-2*(num-0.5))).to_i,0]
+      end
     end
 
     def normalize(in_array)
+      max = max_value
+      in_array.map{|row| row.map{|v| v/max.to_f}}
+    end
 
+    def max_value
+      @max ||= original_grid.map(&:max).max
     end
 
   end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -3,18 +3,57 @@ require 'spec_helper'
 TEST = [
   [1,2,3,4],
   [2,3,4,5],
-  [3,4,5,6]
+  [3,4,5,6],
+  [4,5,6,7]
+]
+
+RESULT = 
+[
+  [0.14285714285714285,0.2857142857142857,0.42857142857142855,0.5714285714285714],
+  [0.2857142857142857,0.42857142857142855,0.5714285714285714,0.7142857142857143],
+  [0.42857142857142855,0.5714285714285714,0.7142857142857143,0.8571428571428571],
+  [0.5714285714285714, 0.7142857142857143, 0.8571428571428571, 1.0]
+]
+
+RGB = [
+  [[0, 72, 182], [0, 145, 109], [0, 218, 36], [36, 218, 0]],
+  [[0, 145, 109], [0, 218, 36], [36, 218, 0], [109, 145, 0]],
+  [[0, 218, 36], [36, 218, 0], [109, 145, 0], [182, 72, 0]],
+  [[36, 218, 0], [109, 145, 0], [182, 72, 0], [255, 0, 0]]
 ]
 
 describe Heatgrid::Grid do
-  describe "#new" do
-    it "saves the original input" do
-      grid = Heatgrid::Grid.new(TEST)
+  let(:grid) {Heatgrid::Grid.new(TEST)}
 
+  describe "#new" do
+    
+    it "saves the original input" do
       expect(grid.original_grid).to eq(TEST)
     end
-  end
 
+    it "finds the maximum value" do
+      expect(grid.max_value).to eq(7)
+    end
+
+    it "calculates the normalized grid" do
+      expect(grid.normalized_grid).to eq(RESULT)
+    end
+
+    it "converts normalized values to rgb" do
+      expect(grid.normalized_to_rgb(0.0)).to eq([0,0,255])
+      expect(grid.normalized_to_rgb(0.1)).to eq([0,51,204])
+      expect(grid.normalized_to_rgb(0.25)).to eq([0,127,127])
+      expect(grid.normalized_to_rgb(0.5)).to eq([0,255,0])
+      expect(grid.normalized_to_rgb(0.75)).to eq([127,127,0])
+      expect(grid.normalized_to_rgb(1.0)).to eq([255,0,0])
+      expect(grid.normalized_to_rgb(0.9)).to eq([204,50,0])
+    end
+
+    it "gets the rgb values" do
+      expect(grid.rgb_grid).to eq(RGB)
+    end
+
+  end
 
 end
 
