2d10fb4709ecc348a938f78c6f38c5abc3bd89dc
Eugene Millerman <e.v.millerman@gmail.com>
2014-03-23 14:22:29 -0400

    Add basic image tests

--- lib/heatgrid/image.rb
+++ lib/heatgrid/image.rb
@@ -2,15 +2,21 @@ require 'rmagick'
 
 module Heatgrid
   class Image
-    attr_accessor :data
+    attr_accessor :data, :row_count, :column_count
+
     def initialize()
 
     end
 
-    def self.fromRgb(data_array)
+    def from_rgb(rgb_array)
+      self.data = rgb_array
+      self.row_count = rgb_array.size
+      self.column_count = rgb_array.first.size
+    end
+
+    def write(filename)
       canvas = Magick::ImageList.new
       canvas.new_image(1000,1000, Magick::HatchFill.new('white','gray90'))
-      
       # (0..9).each do |x|
       #   (0..9).each do |y|
       #     tx,ty,bx,by = x*100,y*100,(x+1)*100,(y+1)*100
@@ -23,7 +29,6 @@ module Heatgrid
       #     rect.draw(canvas)
       #   end
       # end
-
       canvas.write("image.jpg")
     end
 
--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -1,16 +1,21 @@
 require 'spec_helper'
 
-# RGB = [
-#   [[0, 72, 182], [0, 145, 109], [0, 218, 36], [36, 218, 0]],
-#   [[0, 145, 109], [0, 218, 36], [36, 218, 0], [109, 145, 0]],
-#   [[0, 218, 36], [36, 218, 0], [109, 145, 0], [182, 72, 0]],
-#   [[36, 218, 0], [109, 145, 0], [182, 72, 0], [255, 0, 0]]
-# ]
-
 describe Heatgrid::Image do
   let(:image) {Heatgrid::Image.new}  
-  it "initializes with rgb" do
-    Heatgrid::Image.new
+  let(:rgb) do 
+    [
+      [[0, 72, 182], [0, 145, 109], [0, 218, 36], [36, 218, 0]],
+      [[0, 145, 109], [0, 218, 36], [36, 218, 0], [109, 145, 0]],
+      [[0, 218, 36], [36, 218, 0], [109, 145, 0], [182, 72, 0]],
+      [[36, 218, 0], [109, 145, 0], [182, 72, 0], [255, 0, 0]]
+    ]
+  end
+
+
+  it "takes an array of rgb values" do
+    image.from_rgb(rgb)
+    expect(image.row_count).to eq(4)
+    expect(image.column_count).to eq(4)
   end
 end
 
